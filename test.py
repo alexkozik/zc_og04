49d87f9f41f0b70a6c45078532f947701f909547
alexkozik <alex_kozik@hotmail.com>
2025-05-07 19:10:06 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+import main
+
+op1 = 5
+op2 = 6
+print(f"sum of {op1} and {op2} is {main.sum_of(op1,op2)}")
+print(f"sub of {op1} and {op2} is {main.sub_of(op1,op2)}")
+print(f"mul of {op1} and {op2} is {main.mul_of(op1,op2)}")
+print(f"div of {op1} and {op2} is {main.div_of(op1,op2)}")
+print(f"int of div {op1} and {op2} is {main.int_of_div(op1,op2)}")
+print(f"rem of div {op1} and {op2} is {main.rem_of_div(op1,op2)}")
+print(f"inv of {op1} and {op2} is {main.inv_of(op1,op2)}")
